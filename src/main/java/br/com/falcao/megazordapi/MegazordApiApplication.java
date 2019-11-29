b1a8e6cfe4132bd0c0df5557dc1d05b24f68d0e0
Nichollas Falcão <nichollas.moura@mv.com.br>
2019-11-29 10:13:29 -0300

    Kickstart

---
+++ src/main/java/br/com/falcao/megazordapi/MegazordApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.falcao.megazordapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegazordApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MegazordApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/falcao/megazordapi/MegazordApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.falcao.megazordapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MegazordApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
