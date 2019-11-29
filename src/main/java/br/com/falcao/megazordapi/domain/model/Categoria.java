04014bd75eecee9c45168448f6acbe625e8bbf3f
Nichollas Falcão <nichollas.moura@mv.com.br>
2019-11-29 15:08:36 -0300

    hateoas

--- src/main/java/br/com/falcao/megazordapi/domain/model/Categoria.java
+++ src/main/java/br/com/falcao/megazordapi/domain/model/Categoria.java
@@ -1,5 +1,7 @@
 package br.com.falcao.megazordapi.domain.model;
 
+import java.io.Serializable;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -19,7 +21,9 @@ import lombok.Setter;
 @Table(name = "categoria")
 @Getter @Setter @NoArgsConstructor @AllArgsConstructor
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
-public class Categoria {
+public class Categoria extends ResourceSupport implements Serializable {
+
+	private static final long serialVersionUID = 1L;
 
 	@Id
 	@EqualsAndHashCode.Include
